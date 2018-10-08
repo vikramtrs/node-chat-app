89d5a73b21af00487a14f1f234d5ed2662b24b82
Vikram <vikramtrs@gmail.com>
2018-10-07 22:09:11 -0400

    first commit

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+const publicPath = path.join(__dirname, '../public');
+
+app.use(express.static(publicPath));
+
+app.listen(port, () => console.log(`Server is up on ${port}`));
